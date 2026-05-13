fdbdc19fd63b9717c69c774d6c4ba1e18a915267
Hendar Nugraha <manentlabs@gmail.com>
2026-05-13 09:12:53 +0700

    fix(middleware): add authorized callback to fix redirect loop on authenticated routes

--- middleware.js
+++ middleware.js
@@ -1,6 +1,9 @@
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
+  callbacks: {
+    authorized: ({ token }) => !!token, // ← ini yang kurang
+  },
   pages: {
     signIn: "/login",
   },
